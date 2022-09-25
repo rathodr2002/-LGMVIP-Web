12c451d2ffc261d55cd6b9637304d605387c6af2
rathodr2002 <100895212+rathodr2002@users.noreply.github.com>
2022-09-25 12:25:47 +0530

    Add files via upload

---
+++ Task 1 To Do List/main.js
@@ -0,0 +1,22 @@
+let addtodoButton = document.getElementById('addtodo');
+let todocontainer = document.getElementById('todocontainer');
+let inputfield = document.getElementById('inputfield');
+
+addtodoButton.addEventListener('click', function()
+{
+    var paragraph = document.createElement('p') 
+    paragraph.innerText = inputfield.value;
+    paragraph.classList.add('paragraph-styling')
+    todocontainer.appendChild(paragraph);
+    inputfield.value="";
+    paragraph.addEventListener('click', function()
+    {
+        paragraph.style.textDecoration = 'line-through';
+    })
+
+    paragraph.addEventListener('dblclick', function(){
+        todocontainer.removeChild(paragraph);
+    })
+
+    
+})
